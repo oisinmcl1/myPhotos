32128fbc8c50fd0e4892368a22b6cf96ff03c3be
Oisinmcl1 <omclaughlin99@gmail.com>
2025-04-26 19:10:32 +0100

    basic layout, some test photos and small styling

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello world!");
+const photos = [
+    {
+        id: '14KU6FUCvHoMyYqzRpqkrZWC2BaHQQFcp',
+        caption: '21/07/2024 - out back of my home moville'
+    },
+    {
+        id: '1R6iiwbPkI5lq97OPrLYQz7ZK4j2g7ECt',
+        caption: '21/07/2024 - out back of my home moville'
+    }
+];
+
+function loadGallery() {
+    const gallery = document.getElementById('gallery');
+
+    for (let photo of photos) {
+        gallery.innerHTML += `
+        <figure class="photo">
+          <img src="https://drive.google.com/thumbnail?id=${photo.id}"
+               loading="lazy"
+               alt="Photo taken ${photo.caption}">
+          <figcaption>${photo.caption}</figcaption>
+        </figure>
+      `;
+    }
+}
+
+window.onload = loadGallery;
